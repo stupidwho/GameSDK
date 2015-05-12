b71d9803e01370ee260c164c0c178eac600d464b
郑户 <zhenghu@meizu.com>
2015-05-12 14:36:39 +0800

    初步进行产品结构创建，尚未完成

--- SampleApplication/app/src/main/java/me/toufu/sample/platform/model/LicenseInfo.java
+++ SampleApplication/app/src/main/java/me/toufu/sample/platform/model/LicenseInfo.java
@@ -4,7 +4,19 @@ package me.toufu.sample.platform.model;
  * Created by zhenghu on 15-5-8.
  */
 public class LicenseInfo {
+    public byte signature[];
+    public byte content[];
+
     public long timeStamp;
+    public String appId;
+    public String appKey;
+    public String pkgName;
+
+    public String imei;
+    public String sn;
+
+    public ProductInfo productInfo;
+
     public String toString() {
         return null;
     }
--- SampleApplication/app/src/main/java/me/toufu/sample/platform/model/ProductInfo.java
+++ SampleApplication/app/src/main/java/me/toufu/sample/platform/model/ProductInfo.java
@@ -4,4 +4,5 @@ package me.toufu.sample.platform.model;
  * Created by zhenghu on 15-4-29.
  */
 public class ProductInfo {
+    
 }
